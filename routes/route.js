d8c0584a38a52c0b86afdf68f8623415fcfcb8e8
parmarsunny125 <87124328+parmarsunny125@users.noreply.github.com>
2022-12-28 01:25:36 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,10 @@
 import express,{Router} from 'express';
 import { sendData } from '../controller/send-data-controller.js'
-import {addUser,getUsers,getUser,editUser,deleteUser} from '../controller/user-controller.js'
+import {addUser,getUsers,getUser,editUser,deleteUser,home} from '../controller/user-controller.js'
 
 
 const router=express.Router()
+router.post('/',home)
 
 router.post('/senddata', sendData);
 router.post('/add',addUser)
